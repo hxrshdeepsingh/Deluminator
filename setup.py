60b107484e6eeec991d692050e95369431a4d483
Harshdeep Singh <ishu.cxx@gmail.com>
2024-01-22 13:43:38 -0800

    v0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Harshdeep Singh',
     author_email='ishu.cxx@gmail.com',
     license='BSD-3-Clause',
-    long_description=open('Readme.md').read(),
+    long_description=open('README.md').read(),
     description="This package is a reverse shell generator for Windows systems that uses sockets to establish a connection. It is highly capable of bypassing antivirus software. The package includes a control server and comes with a number of useful features.",
     packages=find_packages(include=['Deluminator', 'Deluminator.*']),
     package_data={'Deluminator': ['engine/source']},
